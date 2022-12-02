aa631fdbb4c273d58b8a095e4be53ab174ed7988
sanghee <wjdtkdgml7352@daum.net>
2022-12-02 14:01:13 +0900

    [추가] UserName의 중복 체크 - 2 - repository에 extends 한 User Entity를 생성 - service에서 @RequiredArgsContructor로 생성자 생략

--- src/main/java/com/exercise/com/repository/UserRepository.java
+++ src/main/java/com/exercise/com/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.exercise.com.repository;
 
 
+import com.exercise.com.domain.User;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/com/exercise/com/service/UserService.java
+++ src/main/java/com/exercise/com/service/UserService.java
@@ -1,8 +1,11 @@
 package com.exercise.com.service;
 
+import com.exercise.com.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
     public String join(String userName, String password) {
