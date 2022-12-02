48b04206eb3e7fa7bf70b11e66ea9426d33c02aa
sanghee <wjdtkdgml7352@daum.net>
2022-12-02 14:07:32 +0900

    [추가] UserName의 중복 체크 - 3 - repository : jpa 메소드 생성(findByUserName) - service : findByUserName().ifPresent()로 중복 check

--- src/main/java/com/exercise/com/service/UserService.java
+++ src/main/java/com/exercise/com/service/UserService.java
@@ -10,8 +10,11 @@ public class UserService {
     private final UserRepository userRepository;
     public String join(String userName, String password) {
 
-        // userName의 중복 checki
-
+        // userName의 중복 check
+        userRepository.findByUserName(userName)
+                .ifPresent(user -> {
+                    throw new RuntimeException(userName + "는 이미 존재하는 이름입니다.");
+                });
 
         return "SUCCESS";
     }
