153184eeb6dae113ecf44e8121a98d7924e86e7f
sanghee <wjdtkdgml7352@daum.net>
2022-12-02 18:07:11 +0900

    [추가] controller test 실습 - 회원가입 실패 (exception 추가 전) - 회원가입 성공

---
+++ src/test/java/com/exercise/com/controller/UserControllerTest.java
@@ -0,0 +1,55 @@
+package com.exercise.com.controller;
+
+import com.exercise.com.domain.dto.UserJoinRequest;
+import com.exercise.com.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
+
+@WebMvcTest // mock 테스트
+class UserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    UserService userService; // userService를 MockBean
+
+    @Autowired
+    ObjectMapper objectMapper; // java Object -> json으로 만들어주는 jackson object
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void join() throws Exception {
+        String userName = "sanghee";
+        String password = "hello";
+
+        mockMvc.perform(post("/api/v1/users/join")// post로 호출
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password)))) // httpRequest에 값을 보낼 땐 byte단위로 보낸다.
+                .andDo(print())
+                .andExpect(status().isOk()); // 잘 처리되면 ok
+    }
+    @Test
+    @DisplayName("회원가입 실패 - userName 중복")
+    void join_fail() throws Exception {
+        String userName = "sanghee";
+        String password = "hello";
+        mockMvc.perform(post("/api/v1/users/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password))))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
+
+}
