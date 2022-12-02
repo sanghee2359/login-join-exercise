540f9f83022a1aebd51b48b1c19d8b45ee197625
sanghee <wjdtkdgml7352@daum.net>
2022-12-02 14:02:27 +0900

    [수정] repository - JpaRepository import하기

--- src/main/java/com/exercise/com/repository/UserRepository.java
+++ src/main/java/com/exercise/com/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.exercise.com.repository;
 
 
 import com.exercise.com.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
