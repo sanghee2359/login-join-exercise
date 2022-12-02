21913cbd5ab943cf2ebc195268bd0f4c87fcc6e9
sanghee <wjdtkdgml7352@daum.net>
2022-12-02 13:19:14 +0900

    [추가] UserJoinRequest dto 생성

--- src/main/java/com/exercise/com/domain/dto/UserJoinRequest.java
+++ src/main/java/com/exercise/com/domain/dto/UserJoinRequest.java
@@ -1,4 +1,11 @@
 package com.exercise.com.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class UserJoinRequest {
+    private String userName;
+    private String password;
 }
