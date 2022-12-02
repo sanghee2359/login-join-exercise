372b9e2b3890629a18799d7a7b6b246c6043ef3d
sanghee <wjdtkdgml7352@daum.net>
2022-12-02 13:32:18 +0900

    [수정] join의 return을 ResponseEntity<String>로 임시 변경

--- src/main/java/com/exercise/com/controller/UserController.java
+++ src/main/java/com/exercise/com/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.exercise.com.controller;
 
 import com.exercise.com.domain.dto.UserJoinRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
+    private final UserService userService;
     @PostMapping("/join")
-    public String join(@RequestBody UserJoinRequest dto) {
-//        return Response.success().
+    public ResponseEntity<String> join(@RequestBody UserJoinRequest dto) {
+        return ResponseEntity.ok().body("회원가입이 성공했습니다.");
     }
 }
---
+++ src/main/java/com/exercise/com/service/UserService.java
@@ -0,0 +1,15 @@
+package com.exercise.com.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public String join(String userName, String password) {
+
+        // userName의 중복 checki
+
+
+        return "SUCCESS";
+    }
+}
