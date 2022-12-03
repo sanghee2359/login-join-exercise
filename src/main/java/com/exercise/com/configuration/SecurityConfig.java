e2a84a56a13198c3d1b31f61b8a990ad89f12092
sanghee <wjdtkdgml7352@daum.net>
2022-12-03 20:50:02 +0900

    [추가] Password Encoding해서 저장하기 -3 -Security Config에서 SecurityFilterChain을 @Bean으로 재정의한다.

--- src/main/java/com/exercise/com/configuration/SecurityConfig.java
+++ src/main/java/com/exercise/com/configuration/SecurityConfig.java
@@ -1,4 +1,27 @@
 package com.exercise.com.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean // bean으로 securityFilterchain을 재정의해서 사용한다.
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable() // UI쪽에서 들어오는 걸 disable
+                .csrf().disable() // cross Site에서 disable
+                .cors().and()// cross site에서 도메인이 다를 때 허용
+                .authorizeRequests()
+                .antMatchers("/api/**").permitAll()
+                .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .build();
+    }
 }
